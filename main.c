532514ce643f0a1bcce15ab669c4866fc24f8494
Maaz Ullah <ullahmaa3@gmail.com>
2025-09-08 15:23:30 +0200

    add sds library

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "lib/sds/sds.h"
 
 int main() {
-	printf("Hello, World!\n");
+	sds welcome_msg = sdsnew("Hello, World!");
+
+	printf("%s\n", welcome_msg);
+	sdsfree(welcome_msg);
+
 	return 0;
 }
