2bbcecec833ee46fcd09839d6fe92c60a7052468
Maaz Ullah <ullahmaa3@gmail.com>
2025-09-08 18:11:42 +0200

    add complete list of tokens

--- include/tokens.h
+++ include/tokens.h
@@ -5,6 +5,40 @@
 typedef enum {
 	TOKEN_EOF = 0,
 	TOKEN_ILLEGAL,
+
+	IDENT,
+	INT,
+	STRING,
+
+	ASSIGN,
+	PLUS,
+	MINUS,
+	ASTERISK,
+	SLASH,
+	BANG,
+
+	SEMICOLON,
+	COMMA,
+
+	LBRACE,
+	RBRACE,
+	LPAREN,
+	RPAREN,
+
+	EQ,
+	NEQ,
+	GT,
+	GTEQ,
+	LT,
+	LTEQ,
+
+	LET,
+	RETURN,
+	IF,
+	ELSE,
+	FUNCTION,
+	TRUE,
+	FALSE,
 } TokenType;
 
 typedef struct {
--- src/tokens.c
+++ src/tokens.c
@@ -4,5 +4,39 @@ const char* TokenTypeToString(TokenType type) {
   switch (type) {
     case TOKEN_EOF: return "EOF";
     case TOKEN_ILLEGAL: return "ILLEGAL";
-  }
+
+    case IDENT: return "IDENT";
+    case INT: return "INT";
+    case STRING: return "STRING";
+
+    case ASSIGN: return "=";
+    case PLUS: return "+";
+    case MINUS: return "-";
+    case ASTERISK: return "*";
+    case SLASH: return "/";
+    case BANG: return "!";
+
+    case SEMICOLON: return ";";
+    case COMMA: return ",";
+
+    case LBRACE: return "{";
+    case RBRACE: return "}";
+    case LPAREN: return "(";
+    case RPAREN: return ")";
+
+    case EQ: return "==";
+    case NEQ: return "!=";
+    case GT: return ">";
+    case GTEQ: return ">=";
+    case LT: return "<";
+    case LTEQ: return "<=";
+
+    case LET: return "LET";
+    case RETURN: return "RETURN";
+    case IF: return "IF";
+    case ELSE: return "ELSE";
+    case FUNCTION: return "FUNCTION";
+    case TRUE: return "TRUE";
+    case FALSE: return "FALSE";
+    }
 }
